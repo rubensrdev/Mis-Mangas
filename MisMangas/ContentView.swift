9bba31b8fe3c30ce5567d4782a2ecf6d0a0f65be
Rubén <ruben.sr.apple@gmail.com>
2024-12-24 12:12:27 +0100

    Initial Commit

---
+++ MisMangas/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MisMangas
+//
+//  Created by Rubén Segura Romo on 24/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MisMangas/MisMangasApp.swift
@@ -0,0 +1,17 @@
+//
+//  MisMangasApp.swift
+//  MisMangas
+//
+//  Created by Rubén Segura Romo on 24/12/24.
+//
+
+import SwiftUI
+
+@main
+struct MisMangasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
