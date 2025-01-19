482a49e83985bc8dd67d429b4dc8190e58bc4622
Rubén <ruben.sr.apple@gmail.com>
2025-01-19 11:52:17 +0100

    componentización de manga row ipad

---
+++ MisMangas/View/Devices/iPad/Explore/BestMangaRowIPad.swift
@@ -0,0 +1,32 @@
+//
+//  BestMangaRowIPad.swift
+//  MisMangas
+//
+//  Created by Rubén Segura Romo on 19/1/25.
+//
+import SwiftUI
+
+struct BestMangaRowIPad: View {
+	let manga: Manga
+	var body: some View {
+		NavigationLink(value: manga) {
+			VStack(spacing: 10) {
+				MangaGridCachedImageViewIPad(url: manga.imageURL)
+				HStack {
+					Text(manga.title)
+						.mangaTitleStyle()
+					Text(manga.scoreFormatted)
+						.scoreStyle()
+				}
+			}
+			.padding()
+			.background(.secondaryGray)
+			.clipShape(RoundedRectangle(cornerRadius: 10))
+			.shadow(color: .black.opacity(0.1), radius: 5, x: 0, y: 5)
+		}
+	}
+}
+
+#Preview {
+	BestMangaRowIPad(manga: .preview)
+}
--- MisMangas/View/Devices/iPad/Explore/ExploreViewIpad.swift
+++ MisMangas/View/Devices/iPad/Explore/ExploreViewIpad.swift
@@ -118,23 +118,4 @@ struct ExploreViewIpad: View {
 		.environment(MyCollectionViewModel(repository: RepositoryLocalPreview()))
 }
 
-struct BestMangaRowIPad: View {
-	let manga: Manga
-	var body: some View {
-		NavigationLink(value: manga) {
-			VStack(spacing: 10) {
-				MangaGridCachedImageViewIPad(url: manga.imageURL)
-				HStack {
-					Text(manga.title)
-						.mangaTitleStyle()
-					Text(manga.scoreFormatted)
-						.scoreStyle()
-				}
-			}
-			.padding()
-			.background(.secondaryGray)
-			.clipShape(RoundedRectangle(cornerRadius: 10))
-			.shadow(color: .black.opacity(0.1), radius: 5, x: 0, y: 5)
-		}
-	}
-}
+
